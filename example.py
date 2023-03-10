9c99acaaa9d1ff076184768e28abfadf9bc36e06
M. Said Bilgehan <msaidbilgehan@gmail.com>
2023-03-10 18:16:20 +0300

    Cleanup and added connection counter

--- example.py
+++ example.py
@@ -3,37 +3,52 @@ from time import time
 from Classes.Container import Container_Struct
 # from multiprocessing import cpu_count
 
-number_of_max_worker = 10
+print("")
+print("=== Initialize ===")
+number_of_max_worker = 100
 
 seed(time())
 searched_data = -13 # randint(0, 100)
-node_length = 10  # randint(0, 10000) or cpu_count() * 100
+node_length = 100  # randint(0, 10000) or cpu_count() * 100
 search_node_index = node_length - randint(1, node_length-1)
 
 # Create a container
-container = Container_Struct()
+container = Container_Struct(number_of_max_worker)
+# container.set_Max_Workers(number_of_max_worker)
+
+print("Max Workers:", container.get_Max_Workers())
 
 # Create node_length node layers
-node_layer_1 = container.create_Node(node_length)
+node_layer_1 = container.create_Node(int(node_length/2))
 node_layer_2 = container.create_Node(node_length)
-node_layer_3 = container.create_Node(node_length)
+node_layer_3 = container.create_Node(node_length*2)
+node_layer_4 = container.create_Node(node_length)
 node_layer_last = container.create_Node(node_length)
 
-layer_list = [node_layer_1, node_layer_2, node_layer_3, node_layer_last]
+layer_list = [
+    node_layer_1, 
+    node_layer_2,
+    node_layer_3, 
+    node_layer_4, 
+    node_layer_last
+]
 
-container.connect_Input_Gate_to_Node_Layer(node_layer_1)
-container.connect_Node_Layers(node_layer_1, node_layer_2)
-container.connect_Node_Layers(node_layer_2, node_layer_3)
-container.connect_Node_Layers(node_layer_3, node_layer_last)
-container.connect_Node_Layer_To_Output_Gate(node_layer_3)
+counter_connections = 0
+counter_connections += container.connect_Input_Gate_to_Node_Layer(node_layer_1)
+counter_connections += container.connect_Node_Layers(node_layer_1, node_layer_2)
+counter_connections += container.connect_Node_Layers(node_layer_2, node_layer_3)
+counter_connections += container.connect_Node_Layers(node_layer_3, node_layer_4)
+counter_connections += container.connect_Node_Layers(node_layer_4, node_layer_last)
+counter_connections += container.connect_Node_Layer_To_Output_Gate(node_layer_3)
 
 # Connect the first node to the input gate
-container.connect_Node_As_Ordered()
+# counter_connections_ordered = container.connect_Node_As_Ordered()
 
 print()
 print("=== Node Layers ===")
 print("Node Number:", container.get_Node_Number())
 print("Blocked Node Number:", container.get_Blocked_Node_Number())
+print("Connections:", counter_connections)
 
 for layer in layer_list:
     print("Layer Length:", len(layer))
@@ -45,6 +60,7 @@ print(
 )
 print(f"Looking for data: {searched_data}")
 
+print("===== Sequential Search =====")
 # get the start time
 start_time = time()
 result_queue = container.search(searched_data)
@@ -63,11 +79,10 @@ print("Path Length:", len(result_queue))
 #     else:
 #         print("None", end="-")
 
-print("")
+
+print("===== Multi-Threaded Search =====")
 
 start_time = time()
-container.set_Max_Workers(number_of_max_worker)
-print("Max Workers:", container.get_Max_Workers())
 found_node_list = container.search_Task(searched_data)
 end_time = time()
 elapsed_time = end_time - start_time

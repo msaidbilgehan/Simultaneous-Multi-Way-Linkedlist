53c76c4a650803ea676c779ac6bed8054d4d3961
M. Said Bilgehan <msaidbilgehan@gmail.com>
2023-03-16 19:05:46 +0300

    fixed ituro map example missing node connection issue

--- example_ituro_map_multi_threaded.py
+++ example_ituro_map_multi_threaded.py
@@ -13,7 +13,7 @@ NODE_ROW_LENGTH = 4  # randint(0, 10000) or cpu_count() * 100
 SEARCHED_NODE_INDEX = NODE_COLUMN_LENGTH - randint(1, NODE_COLUMN_LENGTH-1)
 
 # Create a container
-container = Container_Struct(NUMBER_OF_MAX_WORKERS)
+container = Container_Struct(NUMBER_OF_MAX_WORKERS, verbose=True)
 # container.set_Max_Workers(NUMBER_OF_MAX_WORKERS)
 
 print("Max Workers:", container.get_Max_Workers())
@@ -22,13 +22,26 @@ print("Max Workers:", container.get_Max_Workers())
 node_layer_list = list()
 counter_connections = 0
 
-nodes_row_1 = container.create_Node(5)
-nodes_row_2 = container.create_Node(5)
-nodes_row_3 = container.create_Node(5)
-nodes_row_4 = container.create_Node(5)
-
 node_start = container.create_Node(1)[0]
-counter_connections += node_start.connect_Two_Way_Node(nodes_row_4[-1])
+node_start.set_Data("Start")
+
+nodes_row_1 = container.create_Node(5)
+for i, node in enumerate(nodes_row_1):
+    node.set_Data(f"nodes_row_1:{i}")
+
+nodes_row_2 = container.create_Node(5)
+for i, node in enumerate(nodes_row_2):
+    node.set_Data(f"nodes_row_2:{i}")
+    
+nodes_row_3 = container.create_Node(5)
+for i, node in enumerate(nodes_row_3):
+    node.set_Data(f"nodes_row_3:{i}")
+    
+nodes_row_4 = container.create_Node(5)
+for i, node in enumerate(nodes_row_4):
+    node.set_Data(f"nodes_row_4:{i}")
+
+counter_connections += node_start.connect_Node_BiDirection(nodes_row_4[-1])
 
 counter_connections += container.connect_Nodes_As_Sequential(nodes_row_1, bi_direction=True)
 counter_connections += container.connect_Nodes_As_Sequential(nodes_row_2, bi_direction=True)
@@ -40,32 +53,112 @@ counter_connections += container.connect_Nodes_One_O_One(nodes_row_2, nodes_row_
 counter_connections += container.connect_Nodes_One_O_One(nodes_row_3, nodes_row_4, bi_direction=True)
 
 nodes_x = container.create_Node(3)
+for i, node in enumerate(nodes_x):
+    node.set_Data(f"nodes_x:{i}")
+    
 node_x_connection = container.create_Node(1)[0]
-for node in nodes_x:
-    counter_connections += node.connect_Two_Way_Node(node_x_connection)
-counter_connections += node_x_connection.connect_Two_Way_Node(nodes_row_1[-1])
+node_x_connection.set_Data(f"node_x_connection")
+counter_connections += container.connect_Nodes_To_Node(
+    nodes_x, 
+    node_x_connection, 
+    bi_direction=True
+)
+# for node in nodes_x:
+#     counter_connections += node.connect_Node_BiDirection(node_x_connection)
+counter_connections += node_x_connection.connect_Node_BiDirection(nodes_row_1[-1])
 
 nodes_y = container.create_Node(3)
+for i, node in enumerate(nodes_y):
+    node.set_Data(f"nodes_y:{i}")
+    
 node_y_connection = container.create_Node(1)[0]
-for node in nodes_y:
-    counter_connections += node.connect_Two_Way_Node(node_y_connection)
-counter_connections += node_y_connection.connect_Two_Way_Node(nodes_row_2[-1])
+node_y_connection.set_Data(f"node_y_connection")
+counter_connections += container.connect_Nodes_To_Node(
+    nodes_y,
+    node_y_connection,
+    bi_direction=True
+)
+# for node in nodes_y:
+#     counter_connections += node.connect_Node_BiDirection(node_y_connection)
+counter_connections += node_y_connection.connect_Node_BiDirection(nodes_row_2[-1])
 
 nodes_z = container.create_Node(3)
+for i, node in enumerate(nodes_z):
+    node.set_Data(f"nodes_z:{i}")
+
 node_z_connection = container.create_Node(1)[0]
-for node in nodes_z:
-    counter_connections += node.connect_Two_Way_Node(node_z_connection)
-counter_connections += node_z_connection.connect_Two_Way_Node(nodes_row_3[-1])
+node_z_connection.set_Data(f"node_z_connection")
+counter_connections += container.connect_Nodes_To_Node(
+    nodes_z,
+    node_z_connection,
+    bi_direction=True
+)
+# for node in nodes_z:
+#     counter_connections += node.connect_Node_BiDirection(node_z_connection)
+counter_connections += node_z_connection.connect_Node_BiDirection(nodes_row_3[-1])
 
 nodes_t = container.create_Node(3)
+for i, node in enumerate(nodes_t):
+    node.set_Data(f"nodes_t:{i}")
+    
 node_t_connection = container.create_Node(1)[0]
-for node in nodes_t:
-    counter_connections += node.connect_Two_Way_Node(node_t_connection)
-counter_connections += node_t_connection.connect_Two_Way_Node(nodes_row_4[-1])
+node_t_connection.set_Data(f"node_t_connection")
+counter_connections += container.connect_Nodes_To_Node(
+    nodes_t,
+    node_t_connection,
+    bi_direction=True
+)
+# for node in nodes_t:
+#     counter_connections += node.connect_Node_BiDirection(node_t_connection)
+counter_connections += node_t_connection.connect_Node_BiDirection(nodes_row_4[-1])
 
 node_List, input_Gate, output_Gate = container.get_Struct()
 
-counter_connections += node_start.connect_Two_Way_Node(input_Gate)
+counter_connections += input_Gate.connect_Node_BiDirection(node_start)
+counter_connections += output_Gate.connect_Node_BiDirection(
+    input_Gate
+)
+
+# for node in node_List:
+#     if len(node.get_Connected_Node_List()) == 0:
+#         print("ID:", node.get_ID(), "=>", node.get_Data())
+# print("")
+# counter_connections += container.connect_Nodes_To_Node(
+#     nodes_row_1,
+#     output_Gate,
+#     bi_direction=True
+# )
+# counter_connections += output_Gate.connect_Node_BiDirection(
+#     nodes_row_2[0]
+# )
+# counter_connections += output_Gate.connect_Node_BiDirection(
+#     nodes_row_3[0]
+# )
+# counter_connections += container.connect_Nodes_To_Node(
+#     nodes_row_4[:-1],
+#     output_Gate,
+#     bi_direction=True
+# )
+# counter_connections += container.connect_Nodes_To_Node(
+#     nodes_x,
+#     output_Gate,
+#     bi_direction=True
+# )
+# counter_connections += container.connect_Nodes_To_Node(
+#     nodes_y,
+#     output_Gate,
+#     bi_direction=True
+# )
+# counter_connections += container.connect_Nodes_To_Node(
+#     nodes_z,
+#     output_Gate,
+#     bi_direction=True
+# )
+# counter_connections += container.connect_Nodes_To_Node(
+#     nodes_t,
+#     output_Gate,
+#     bi_direction=True
+# )
 
 
 print()
@@ -93,6 +186,9 @@ found_node_list = container.search_Task(
     -1,
     True
 )
+# found_node_list = container.search(
+#     SEARCHED_DATA
+# )
 end_time = time()
 elapsed_time = end_time - start_time
 print('Execution time:', elapsed_time, 'seconds for',
@@ -104,7 +200,9 @@ print('Execution time:', elapsed_time, 'seconds for',
 #       elapsed_time / container.get_Node_Number(), "ms")
 
 print(f"Found {len(found_node_list)} Node")
-print("IDs:", [node.get_ID() for node in found_node_list])
+
+for i, found_list in enumerate(found_node_list):
+    print(i, "- IDs:", [node.get_ID() for node in found_list])
 
 _, input_gate, _ = container.get_Struct()
 for found_node in found_node_list:
